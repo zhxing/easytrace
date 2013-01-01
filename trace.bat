87bacccfaa725b2be5ae667de740b024830f602c
zhxing <84858302@163.com>
2013-01-01 12:38:06 +0800

    add jar

---
+++ trace.bat
@@ -0,0 +1,12 @@
+@echo off
+if not exist "./easytrace-jar-with-dependencies.jar" goto noTraceHome
+
+if "%JAVA_HOME%" == "" goto noJavaHome
+  "%JAVA_HOME%/bin/java" -Dtrace.log.level= -Dtrace.log.path= -cp "./easytrace-jar-with-dependencies.jar;%JAVA_HOME%/lib/tools.jar" cn.zhxing.trace.client.Main %*
+  goto end
+:noJavaHome
+  echo Please set JAVA_HOME before running this script
+  goto end
+:noTraceHome
+  echo Please put the easytrace-jar-with-dependencies.jar on the current path  before running this script
+:end
