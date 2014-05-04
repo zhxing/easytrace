ca471585bf96168e1fd56d9a77dbf08d1e5ed53a
zhxing <84858302@163.com>
2014-05-04 19:34:50 +0800

    thread/top command

--- trace.sh
+++ trace.sh
@@ -2,7 +2,7 @@
 
 if [ "${JAVA_HOME}" != "" ]; then
     TOOLS_JAR="${JAVA_HOME}/lib/tools.jar"
-    echo ${TOOLS_JAR}
+#    echo ${TOOLS_JAR}
     ${JAVA_HOME}/bin/java -Dtrace.log.level= -Dtrace.log.path= -cp ./easytrace-jar-with-dependencies.jar:${JAVA_HOME}/lib/tools.jar cn.zhxing.trace.client.Main $*
 else
     echo "Please set JAVA_HOME before running this script"
