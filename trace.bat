175f7346b54d4243e07d437d9fd98ab71368dd2c
zhxing <84858302@163.com>
2014-05-04 19:39:02 +0800

    script modify

--- trace.bat
+++ trace.bat
@@ -2,7 +2,7 @@
 if not exist "./easytrace-jar-with-dependencies.jar" goto noTraceHome
 
 if "%JAVA_HOME%" == "" goto noJavaHome
-  "%JAVA_HOME%/bin/java" -Dtrace.log.level= -Dtrace.log.path= -cp "./easytrace-jar-with-dependencies.jar;%JAVA_HOME%/lib/tools.jar" cn.zhxing.trace.client.Main %*
+  "%JAVA_HOME%/bin/java" -Dtrace.log.level= -Dtrace.log.path= -cp "./easytrace-jar-with-dependencies.jar;%JAVA_HOME%/lib/tools.jar" com.github.zhxing.easytrace.client.Main %*
   goto end
 :noJavaHome
   echo Please set JAVA_HOME before running this script
